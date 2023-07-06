a68a09332ce7da8f85f36e6a2b6592242763376b
Alex Tamayo <catd00@gmail.com>
2023-07-05 20:11:32 -0700

    Added a file to hold the connection logic and exported and imported modules accordingly

---
+++ client.js
@@ -0,0 +1,38 @@
+/*
+
+The credentials for it are:
+
+Web view: http://165.227.47.243:8081/
+IP for client : 165.227.47.243
+Port for client : 50541
+
+Use the IP and PORT for your snek client, use the web view to see the snake UI 🐍
+
+Good Luck 🐍 🐍 🐍
+
+*/
+
+
+const net = require("net");
+
+// establish a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  // interpret income data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+// connect();
+
+module.exports = {connect};
