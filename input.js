29eea174dd5566c951a97312d36cf5f1646cc6ea
Alex Tamayo <catd00@gmail.com>
2023-07-05 22:45:15 -0700

    Added random messages to some keys.

--- input.js
+++ input.js
@@ -21,18 +21,42 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
+
+  // MOVE KEYS
   if (key === '\u001b[A') {
     connection.write("Move: up");
   }
+
   if (key === '\u001b[B') {
     connection.write("Move: down");
   }
+
   if (key === '\u001b[C') {
     connection.write("Move: right");
   }
+
   if (key === '\u001b[D') {
     connection.write("Move: left");
   }
+
+  // MESSAGES KEY
+  if (key === 'A' || key === 'a') {
+    connection.write("Say: GET TO THE CHOPPA, NOWH!!");
+  }
+  
+  if (key === 'S' || key === 's') {
+    connection.write("Say: SSSSSSSSHHH");
+  }
+  
+  if (key === 'W' || key === 'w') {
+    connection.write("Say: Alex number 1");
+  }
+  
+  if (key === 'D' || key === 'd') {
+    connection.write("Say: I'm Rick James B***ch!");
+  }
+  
+
 };
 
 const setupInput = function(conn) {
